4980fd917b7bcd15ab5305417d9af94dfc2365a9
carlos.rodrigues <carlos.rodrigues@seventh.com.br>
2026-09-08 03:53:06 -0300

    fix: restore Quasar 3 runtime compatibility

--- src/router/index.js
+++ src/router/index.js
@@ -17,9 +17,9 @@ import routes from './routes'
  */
 
 export default defineRouter((/* { store, ssrContext } */) => {
-  const createHistory = process.env.SERVER
+  const createHistory = import.meta.env.SSR
     ? createMemoryHistory
-    : process.env.VUE_ROUTER_MODE === 'history'
+    : import.meta.env.QUASAR_VUE_ROUTER_MODE === 'history'
       ? createWebHistory
       : createWebHashHistory
 
@@ -30,7 +30,7 @@ export default defineRouter((/* { store, ssrContext } */) => {
     // Leave this as is and make changes in quasar.conf.js instead!
     // quasar.conf.js -> build -> vueRouterMode
     // quasar.conf.js -> build -> publicPath
-    history: createHistory(process.env.VUE_ROUTER_BASE),
+    history: createHistory(import.meta.env.QUASAR_VUE_ROUTER_BASE),
   })
 
   return Router
